a77d8287164f7e564b3ecafb80d0d404d1ff062a
thanhquang2004 <quangthanh7204@gmail.com>
2024-12-22 22:13:42 +0700

    Add api get detail

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,11 +1,13 @@
 import { StatusCodes } from "http-status-codes";
 import Joi from "joi";
 import ApiError from "~/utils/ApiError";
+import { BOARD_TYPES } from "~/utils/constants";
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(30).trim().strict(),
     description: Joi.string().max(30).trim().strict(),
+    type: Joi.string().valid(BOARD_TYPES.PRIVATE, BOARD_TYPES.PUBLIC).required(),
   });
 
   try {
