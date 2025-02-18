cec9acd536b6585183612fa0ef428790305e94a0
thanhquang2004 <quangthanh7204@gmail.com>
2025-02-18 21:30:50 +0700

    Update CORS

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,9 +5,11 @@ import { WHITELIST_DOMAINS } from "~/utils/constants";
 
 export const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin && env.BUILD_MODE === "dev") {
+    if ( env.BUILD_MODE === "dev") {
       return callback(null, true);
     }
+
+    
 
     if (WHITELIST_DOMAINS.includes(origin)) {
       return callback(null, true);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+export const WHITELIST_DOMAINS = [];
 
 export const BOARD_TYPES = {
   PUBLIC: "public",
